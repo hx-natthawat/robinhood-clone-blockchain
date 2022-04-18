a1b6faa64eb4f5c74985b28ced10804036e28b49
hx-natthawat <natthawat@harmonyx.co>
2022-04-18 10:24:58 +0700

    frontend

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,48 @@
 import Head from 'next/head'
 import Image from 'next/image'
+
+// Components
+import Header from '../components/Header'
+import PortfolioChart from '../components/PortfolioChart'
+import BuyTokens from '../components/BuyTokens'
+
+// Icon
 import { BiDotsHorizontalRounded } from 'react-icons/bi'
 import { AiOutlinePlus } from 'react-icons/ai'
 
 const styles = {
   wrapper: 'w-screen h-screen flex flex-col',
   mainContainer: 'w-2/3 h-full m-auto flex mt-16',
-  leftMain: 'flex flex-col w-3/4 h-full p-6 overflow-y-scroll',
-  portfolioAmountContainer: 'flex flex-col',
+  leftMain: 'flex flex-col w-3/4 h-full  p-6 overflow-y-scroll',
+  portfolioAmountContainer: 'flex flex-col ',
   portfolioAmount: 'text-white text-4xl',
   portfolioPercent: 'text-white font-bold text-sm',
   pastHour: 'text-gray-400',
   chartContainer:
     'text-5xl flex justify-center w-full h-1/3 text-white mt-11 mb-11',
   buyingPowerContainer:
-    'w-full norder-t mb-24 border-b h-16 border-[#30363b] flex justify-between',
+    'w-full border-t mb-24 border-b h-16 border-[#30363b] flex justify-between items-center p-4',
   buyingPowerTitle: 'text-white font-bolder text-lg',
   buyingPowerAmount: 'text-white font-bolder text-xl',
   notice: 'flex border border-[#30363b] mx-11 my-4 p-5 flex-col flex-1',
   noticeContainer: 'flex-1',
   noticeTitle: 'text-gray-500',
   noticeMessage: 'text-white font-bold',
+  noticeCTA: 'font-bold text-green-500 cursor-pointer mt-5',
   rightMain:
-    'flex flex-col flex-1 h-4.5 bg-[#1E2123] mt-6 rounded-lg overflow-y-scroll noScroll',
+    'flex flex-col flex-1 h-4/5 bg-[#1E2123] mt-6 rounded-lg overflow-y-scroll noScroll',
   rightMainItem: 'flex items-center text-white p-5 border-b border-[#30363b]',
-  itemTitle: 'cursor-pointer text-xl',
-  moreOptions: '',
+  ItemTitle: 'flex-1 font-bold',
+  moreOptions: 'cursor-pointer text-xl',
 }
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
       {/* header */}
+      <header>
+        <Header />
+      </header>
 
       <div className={styles.mainContainer}>
         {/* LeftMain */}
@@ -45,7 +56,7 @@ export default function Home() {
           </div>
           <div>
             <div className={styles.chartContainer}></div>
-            {/* PortfolioChart */}
+            <PortfolioChart />
           </div>
 
           <div className={styles.buyingPowerContainer}>
@@ -58,7 +69,7 @@ export default function Home() {
               <div className={styles.noticeMessage}>
                 Transfer your fund here.
               </div>
-              {/* <BuyToken /> */}
+              <BuyTokens />
             </div>
           </div>
           {/* <Notice /> */}
